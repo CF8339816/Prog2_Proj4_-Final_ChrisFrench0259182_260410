4c0fbda97673c814947208bbd705a9b1278d704a
CF8339816 <chris_french_3dga@hotmail.com>
2026-03-30 10:30:06 -0300

    working on rectangle triggers aka triggers shaped like triangles.

---
+++ Prog2_Proj4_ Final_ChrisFrench0259182_260410/RecTrigX.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prog2_Proj3_beta_ChrisFrench0259182_260324
+{
+    internal class RecTrig 
+    {
+
+
+
+        public RecTrigX (string Name, ConsoleColor fgColor, ConsoleColor bgColor, (int, int) Rect_min_max_x, (int, int) Rect_min_max_y) : ( Name,  fgColor,  bgColor, (13, 55) , (0, 25) )
+        {
+
+        }
+
+    }
+}
