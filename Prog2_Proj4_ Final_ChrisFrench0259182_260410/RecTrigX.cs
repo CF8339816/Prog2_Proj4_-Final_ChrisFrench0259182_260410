0c8c249d50840c6a0ea403ce20ff306da76b1022
CF8339816 <chris_french_3dga@hotmail.com>
2026-04-01 10:33:36 -0300

    working on rectangle trigger class

--- Prog2_Proj4_ Final_ChrisFrench0259182_260410/RecTrigX.cs
+++ Prog2_Proj4_ Final_ChrisFrench0259182_260410/RecTrigX.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Prog2_Proj4_Final_ChrisFrench0259182_260410;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,15 +7,36 @@ using System.Threading.Tasks;
 
 namespace prog2_Proj3_beta_ChrisFrench0259182_260324
 {
-    internal class RecTrig 
+    internal class RecTrig
     {
 
 
+        public bool IsTriggered = false;
 
-        public RecTrigX (string Name, ConsoleColor fgColor, ConsoleColor bgColor, (int, int) Rect_min_max_x, (int, int) Rect_min_max_y) : ( Name,  fgColor,  bgColor, (13, 55) , (0, 25) )
+        public RecTrig(string Name, ConsoleColor fgColor, ConsoleColor bgColor, (int, int) Rect_min_max_x, (int, int) Rect_min_max_y) : (Name, fgColor, bgColor, Rect_min_max_x, Rect_min_max_y)
         {
 
         }
+    
+       public void ActivateTrigger()
+        {
+        if (GameManager.player.plX)
+            {
 
-    }
+            }
+        
+
+        }
+
+
+
+
+    
+
+    
 }
+
+
+
+
+
