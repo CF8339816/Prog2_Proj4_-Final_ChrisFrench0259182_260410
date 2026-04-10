c12aaccd585599d09c1c2c2f69e9646344a09e0d
CF8339816 <chris_french_3dga@hotmail.com>
2026-04-10 19:03:40 -0300

    Captive class changes verified and tested working

--- Prog2_Proj4_ Final_ChrisFrench0259182_260410/rectangle.cs
+++ Prog2_Proj4_ Final_ChrisFrench0259182_260410/rectangle.cs
@@ -11,18 +11,16 @@ namespace prog2_Proj3_beta_ChrisFrench0259182_260324
 
         public string _name { get; set; }
 
-        //public ConsoleColor _bgColor { get; set; }
-        //public ConsoleColor _fgColor { get; set; }
+        
         public static bool _isTriggered { get; set; }
         public int _min_x { get; set; }
         public int _min_y { get; set; }
         public int _max_x { get; set; }
         public int _max_y { get; set; }
-        protected rectangle(string Name, /*ConsoleColor fgColor, ConsoleColor bgColor,*/ int Rect_min_x, int Rect_max_x, int Rect_min_y, int Rect_max_y, bool IsTriggered)
+        protected rectangle(string Name,  int Rect_min_x, int Rect_max_x, int Rect_min_y, int Rect_max_y, bool IsTriggered)
         {
             _name = Name;
-            //_bgColor = bgColor;
-            //_fgColor = fgColor;
+          
             _min_x = Rect_min_x;
             _min_y = Rect_min_y;
             _max_x = Rect_max_x;
